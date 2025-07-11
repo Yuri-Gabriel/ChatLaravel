82cbb7e3ebb4de58bbd4226ff43d028215d96928
Yuri-Gabriel <yurigabriel.f1012@gmail.com>
2025-07-10 21:41:27 -0300

    adicionei uns try/catch e acho que o backend já ta quase pronto

--- app/Http/Controllers/GrupoController.php
+++ app/Http/Controllers/GrupoController.php
@@ -5,27 +5,35 @@ namespace App\Http\Controllers;
 use App\Dto\GrupoDTO;
 use App\Http\Service\GrupoService;
 use App\Models\Grupo;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class GrupoController extends Controller {
     public function createGroup(Request $request): JsonResponse {
-        $grupoDto = GrupoDTO::fromRequest($request);
+        try {
+            $grupoDto = GrupoDTO::fromRequest($request);
 
-        $saved = GrupoService::save(
-            Grupo::fromDTO($grupoDto)
-        );
+            $saved = GrupoService::save(
+                Grupo::fromDTO($grupoDto)
+            );
 
-        if (!$saved) {
+            if (!$saved) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Erro ao criar o grupo'
+                ], JsonResponse::HTTP_BAD_REQUEST);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Grupo criado com sucesso'
+            ], JsonResponse::HTTP_CREATED);
+        } catch (Exception $err) {
             return response()->json([
                 'success' => false,
-                'message' => 'Erro ao criar o grupo'
-            ], JsonResponse::HTTP_BAD_REQUEST);
+                'message' => $err->getMessage()
+            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        return response()->json([
-            'success' => true,
-            'message' => 'Grupo criado com sucesso'
-        ], JsonResponse::HTTP_CREATED);
     }
 }
--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -5,29 +5,36 @@ namespace App\Http\Controllers;
 use App\Dto\UsuarioDTO;
 use App\Http\Service\UsuarioService;
 use App\Models\Usuario;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller {
 
     public function createUser(Request $request): JsonResponse {
+        try {
+            $usuarioDTO = UsuarioDTO::fromRequest($request);
 
-        $usuarioDTO = UsuarioDTO::fromRequest($request);
+            $saved = UsuarioService::save(
+                Usuario::fromDTO($usuarioDTO)
+            );
 
-        $saved = UsuarioService::save(
-            Usuario::fromDTO($usuarioDTO)
-        );
+            if (!$saved) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Erro ao salvar usuário'
+                ], JsonResponse::HTTP_BAD_REQUEST);
+            }
 
-        if (!$saved) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Usuário salvo com sucesso'
+            ], JsonResponse::HTTP_CREATED);
+        } catch (Exception $err) {
             return response()->json([
                 'success' => false,
-                'message' => 'Erro ao salvar usuário'
-            ], JsonResponse::HTTP_BAD_REQUEST);
+                'message' => $err->getMessage()
+            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
-
-        return response()->json([
-            'success' => true,
-            'message' => 'Usuário salvo com sucesso'
-        ], JsonResponse::HTTP_CREATED);
     }
 }
